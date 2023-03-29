88dbac5dbe4a763803925922e2bde01d58cd61b6
Hoang-Van-Thai <124911739+Hoang-Van-Thai@users.noreply.github.com>
2023-03-29 17:08:43 +0700

    Add files via upload

---
+++ CTDLGTB1.05.cpp
@@ -0,0 +1,26 @@
+﻿//Cho mảng 1 chiều các số thực.
+//Hãy viết hàm đệ quy đếm số lượng giá trị dương có trong mảng
+#include<iostream>
+using namespace std;
+int dem(float arr[], int n, int i = 0, int kq = 0)
+{
+    if (n == 0)
+        return 0;
+    if (i == n)
+    {
+        return kq;
+    }
+    if (arr[i] > 0)
+    {
+        kq++;
+    }
+    return dem(arr, n, i+1, kq);
+}
+
+int main()
+{
+    float arr[] = { -1, 2, 3,- 4, -5, -6, -7, 8, 9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << dem(arr, n);
+    return 0;
+}
